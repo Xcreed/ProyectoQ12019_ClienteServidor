ce5798d106d275fbddc95f46a20719df2c00057b
JP <jpm311296@gmail.com>
2019-04-12 22:10:51 -0600

    Add Operacion

---
+++ NetBeansProjects/Proyecto/src/BL/Operacion.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BL;
+
+/**
+ *
+ * @author creed
+ */
+public class Operacion extends Fraccion {
+    
+    private Fraccion fraccion2;
+    
+    public Operacion(){
+        
+    }
+    
+    public Operacion(String fraccion1, String fraccion2) {
+        super(fraccion1);
+        this.fraccion2 = new Fraccion(fraccion2);
+        
+    }
+    
+}
