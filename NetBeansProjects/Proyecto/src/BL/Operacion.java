afa902e7cb51e5b75dc1beed2408ea42b9181831
Marcela Ortega <damarce135@gmail.com>
2019-04-12 22:16:24 -0600

    Agrega operacion

---
+++ NetBeansProjects/Proyecto/src/BL/Operacion.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BL;
+
+/**
+ *
+ * @author damar
+ */
+public class Operacion extends Fraccion {
+    
+    Fraccion fraccion2 = new Fraccion();
+    
+    
+    
+}
