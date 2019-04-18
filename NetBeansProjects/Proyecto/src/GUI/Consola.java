d964a27de0fa441ade00cb19d5e1a70b18054d30
JP <jpm311296@gmail.com>
2019-04-17 19:16:00 -0600

    Falta cerrar bitacora cuando el archivo es vacio.

---
+++ NetBeansProjects/Proyecto/src/GUI/Consola.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author creed
+ */
+public class Consola {
+    
+    InputStreamReader isr = new InputStreamReader(System.in);
+    BufferedReader teclado = new BufferedReader(isr);
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
